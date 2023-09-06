b2299e1d4ef05abf0aafeae733a499b7eb93c4c3
Tony <72665660+rwoss@users.noreply.github.com>
2023-09-06 22:22:41 +0800

    add dict

--- AutoCheck.sh
+++ AutoCheck.sh
@@ -15,10 +15,18 @@ echo "最后检查的结果: $substring"
 # 查找与获取的字符匹配的行的行号
 line_number=$(grep -n "$substring" dict/test | cut -d ":" -f1)
 
-if [[ -n "$line_number" ]]; then
-    # 删除与获取的字符匹配的行及其以上的所有行
-    sed -i "1,$line_number d" dict/test
-    echo "已删除匹配的行及其以上的内容"
-else
+if [[ -z "$line_number" ]]; then
     echo "未找到匹配的字符"
+    exit 1
 fi
+
+# 检查文件是否为空
+file_size=$(wc -c < dict/test)
+if [[ $file_size -eq 0 ]]; then
+    echo "文件为空"
+    exit 1
+fi
+
+# 删除与获取的字符匹配的行及其以上的所有行
+sed -i "1,$line_number d" dict/test
+echo "已删除匹配的行及其以上的内容"
